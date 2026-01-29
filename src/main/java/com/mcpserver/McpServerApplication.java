a4f4d8c52a52215870925a86ee31c4618a2d7ed6
MarcHerilala <hei.marc.3@gmail.com>
2026-01-29 19:49:52 +0300

    add configuration

--- src/main/java/com/mcpserver/McpServerApplication.java
+++ src/main/java/com/mcpserver/McpServerApplication.java
@@ -1,7 +1,11 @@
 package com.mcpserver;
 
+import com.mcpserver.service.WeatherService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class McpServerApplication {
@@ -9,5 +13,8 @@ public class McpServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(McpServerApplication.class, args);
     }
-
+    @Bean
+    ToolCallbackProvider weatherTools(WeatherService weatherService) {
+        return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+    }
 }
--- src/main/java/com/mcpserver/service/WeatherService.java
+++ src/main/java/com/mcpserver/service/WeatherService.java
@@ -1,5 +1,7 @@
 package com.mcpserver.service;
 
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -13,5 +15,23 @@ public class WeatherService {
                 .defaultHeader("Accept","Application/geo+json")
                 .defaultHeader("User-agent","WeatherApiClient/1.0 hei.marc.3@gmail.com")
                 .build();
+    }
+    @Tool(description = "Get the weather for a specific lon/lat")
+    public String getWeatherForecastByLocation(String lon, String lat){
+        HttpHeaders headers = new HttpHeaders();
+        //to do write the api call
+        return "";
+    }
+    @Tool(description = "Get weather alerts for a US state")
+    public String getAlerts(
+            @ToolParam(description = "Two-letter US state code (e.g. CA, NY)") String state
+    ) {
+        // Returns active alerts including:
+        // - Event type
+        // - Affected area
+        // - Severity
+        // - Description
+        // - Safety instructions
+        return "";
     }
 }
