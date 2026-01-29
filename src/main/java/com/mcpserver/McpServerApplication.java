2b4e70bb55a040312a10b6af551620b7a6ce6a17
MarcHerilala <hei.marc.3@gmail.com>
2026-01-29 20:20:28 +0300

    feat:implemnt the client rest

--- src/main/java/com/mcpserver/McpServerApplication.java
+++ src/main/java/com/mcpserver/McpServerApplication.java
@@ -1,8 +1,7 @@
 package com.mcpserver;
 
-import com.mcpserver.service.WeatherService;
-import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import com.mcpserver.service.MovieService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +12,20 @@ public class McpServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(McpServerApplication.class, args);
     }
-    @Bean
+   /* @Bean
     ToolCallbackProvider weatherTools(WeatherService weatherService) {
         return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
-    }
+    }*/
+   @Bean
+   CommandLineRunner testMovieService(MovieService movieService) {
+       return args -> {
+           System.out.println("=== Test getMovieByTitle ===");
+           String movie = movieService.getMovieByTitle("Inception");
+           System.out.println(movie);
+
+           System.out.println("=== Test searchMovies ===");
+           String search = movieService.searchMovies("Star Wars");
+           System.out.println(search);
+       };
+   }
 }
---
+++ src/main/java/com/mcpserver/service/MovieService.java
@@ -0,0 +1,53 @@
+package com.mcpserver.service;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
+
+@Service
+public class MovieService {
+    private final RestClient restClient;
+    private  String apiKey ;;
+    public MovieService(@Value("${omdb.api.key}") String apiKey) {
+        this.apiKey = apiKey;
+        this.restClient=RestClient.builder()
+                .baseUrl("https://www.omdbapi.com")
+                .defaultHeader("Accept","Application/json")
+              //  .defaultHeader("User-agent","WeatherApiClient/1.0 hei.marc.3@gmail.com")
+                .build();
+    }
+    @Tool(description = "Retrieve film or serie information by titles")
+    public String getMovieByTitle(
+            @ToolParam(description = "Titre du film ou de la série") String title
+    ) {
+        try {
+            String url = String.format("/?apikey=%s&t=%s", apiKey, title.replace(" ", "+"));
+            return restClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .body(String.class);
+        } catch (RestClientException e) {
+            return "{\"error\":\"Impossible de récupérer les données\"}";
+        }
+    }
+
+    @Tool(description = "Recherche plusieurs films ou séries par mot clé")
+    public String searchMovies(
+            @ToolParam(description = "Mot clé pour la recherche") String keyword
+    ) {
+        try {
+            String url = String.format("/?apikey=%s&s=%s", apiKey, keyword.replace(" ", "+"));
+            return restClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .body(String.class)
+                    ;
+
+        } catch (RestClientException e) {
+            return "{\"error\":\"Impossible de récupérer les résultats\"}";
+        }
+    }
+}
